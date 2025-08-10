b1e8666b8a5b80ab62e038c107f4a99003b84c21
C3Pi0-bit <nodetestnet13@gmail.com>
2025-08-10 15:26:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function init() {
     walletconnect: {
       package: window.WalletConnectProvider.default,
       options: {
-        infuraId: "YOUR_INFURA_ID"  // <- заміни на свій Infura Project ID
+        infuraId: "499eccaaa1c34321be3edd18295da9fa"  // <- заміни на свій Infura Project ID
       }
     }
   };
